5602c9c19226528cd73ae55d14e645df5d46f5a0
Maanas Krishna VSL <vslk.maanas@gmail.com>
2025-01-24 09:22:03 +0530

    Added new files to the repo.

---
+++ Java_File.java
@@ -0,0 +1,13 @@
+public class Java_File {
+    public static void main(String[] args) {
+        // Define two numbers directly
+        double num1 = 10.5;
+        double num2 = 5.5;
+
+        // Calculate the sum
+        double sum = num1 + num2;
+
+        // Display the result
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
